ac23553664af664ed8d09bdac19ab9ac3587c96a
hmn3146 <hmn3146@mavs.uta.edu>
2026-06-16 20:01:39 -0500

    continue day 07 functions practice

--- 01-c-basics/day07-functions/practice/practice03_return_value.c
+++ 01-c-basics/day07-functions/practice/practice03_return_value.c
@@ -114,6 +114,7 @@ average = calculateAverage(total);
 For grade:
 
 printGrade(average);
+
 Compile and run
 cd ~/Desktop/c-learning-journey/01-c-basics/day07-functions/practice
 gcc practice03_return_value.c -o practice03_return_value
@@ -123,17 +124,37 @@ gcc practice03_return_value.c -o practice03_return_value
 
 #include <stdio.h>
 
-double calculateAverage(int total) {
-    total = score1 + score2 +score3
-} return total;
+int addThreeScores(int a, int b, int c) {
+    int total = a + b + c;
+    return total;
+}
+
+double calculateAverage(int sum) {
+    int average = sum / 3.0;
+    return average;
+}
+
+void printGrade (double average) {
+    if (average > 90) {
+        printf("Grade: A\n");
+    } else if (average > 80){
+        printf("Grade: B\n");
+    } else if (average > 70){
+        printf("Grade: C\n");
+    } else {
+        printf("Grade: F\n");
+    }
+}
+
 int main() {
 
     int studentCount;
-    int score1;
-    int score2;
-    int score3;
+    int a;
+    int b;
+    int c;
     char name[50];
-
+    int sum = 0;
+    int average;
 
     printf("How many students: ");
     scanf("%d", studentCount);
@@ -143,16 +164,22 @@ int main() {
         scanf("%s", name);
 
         printf("Enter score 1: ");
-        scanf("%d", &score1);
+        scanf("%d", &a);
 
         printf("Enter score 2: ");
-        scanf("%d", &score2);
+        scanf("%d", &b);
 
         printf("Enter score 3: ");
-        scanf("%d", &score3);
+        scanf("%d", &c);
+
+        sum = addThreeScores( a, b, c);
+        average = calculateAverage(sum);
 
         printf("\n--- Student Result---\n");
         printf("Name: %s", name);
+        printf("Total: %d", sum);
+        printf("Average: %.2lf", average);
+        printGrade(average);
     
     }
     
