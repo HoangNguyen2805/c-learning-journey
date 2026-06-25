5b691114b5b1e1ca04dd3e2bb4ce0bf63a556fcd
Hoang Nguyen <hmn3146@mavs.uta.edu>
2026-06-25 00:34:57 -0500

    Finish practice01 cumulative break/continue

--- chapter03-control-flow/3.7-break-continue/practice/practice01_cumulative_break_continue.c
+++ chapter03-control-flow/3.7-break-continue/practice/practice01_cumulative_break_continue.c
@@ -92,7 +92,67 @@ Rules:
 
 int main() {
 
-    
+  int choice;
+  int quantity;
+  int number;
+  
 
-    return 0;
+
+  do {
+
+    printf("\nMenu:\n"
+          "1 - Process a batch of numbers\n"
+          "0 - Quit\n");
+    printf("Enter: ");
+    scanf("%d", &choice);
+
+    switch(choice){
+      case 1:
+        printf("\nHow many number you want to enter ?");
+        scanf("%d", &quantity);
+
+        int accepted = 0;
+        int even = 0;
+        int odd = 0;
+        int sum = 0;
+        for ( int i = 1; i <= quantity ; i++ ){
+          printf("\nEnter number %d:", i);
+          scanf("%d", &number);
+
+          if(number < 0){
+            printf("Negative number skipped.\n");
+            continue;
+          } else if (number == 0) {
+            printf("Zero entered. Stopping early.\n");
+            break;
+          } else {
+              accepted++;
+              
+              if(number %2 == 0){
+              even++;
+            } else {
+              odd++;
+            }
+            sum = sum + number;
+          }
+        }
+        printf("\nOutput:\n");
+        printf("Accepted numbers: %d\n", accepted);
+        printf("Even number: %d\n", even);
+        printf("Odd number: %d\n", odd);
+        printf("Sum: %d\n", sum);
+        break;
+
+      case 0:
+      printf("Quitting\n");
+      break;
+
+      default:
+      printf("Invalid choice, try again!\n");
+      break;
+    }
+
+  } while (choice != 0);
+
+  return 0;
 }
