4133b1e07b7fc0f0630a16a39f431681176d3f00
hmn3146 <hmn3146@mavs.uta.edu>
2026-07-15 14:05:34 -0500

    Complete Kadane's Algorithm practice - max subarray sum

--- chapter05-pointers-and-arrays/5. basic-array/practice/Practice_Basic_Arrays_2_Kadane's_Algorithm_(Max_Subarray_Sum).c
+++ chapter05-pointers-and-arrays/5. basic-array/practice/Practice_Basic_Arrays_2_Kadane's_Algorithm_(Max_Subarray_Sum).c
@@ -22,7 +22,33 @@ You must find which contiguous chunk gives the maximum sum.
 
 int main() {
 
-    
+    int size;
+    printf("You can put something like arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]\n");
+    printf("How many element is in your array ?\n");
+    scanf("%d", &size);
+    int arr[size];
 
+    for(int i = 0; i < size; i++){
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    int currentSum = arr[0];               // loop will start at index 1 not 0
+    int maxSum = arr[0];                   // so set both = arr index 0
+    
+    for(int i = 1; i < size; i++){              // Kadane's Algorithm 
+        if (currentSum < 0){                    // If currentSum is negative
+            currentSum = arr[i];                // Reset case bby set currentSum = arr[1]
+        } else {                                // Else arr > 0 , positive
+            currentSum = currentSum + arr[i];   // Extend the case by adding currentSum to current array value
+        }
+
+        if (currentSum > maxSum) {              // At any point currentSum get bigger than max sum
+            maxSum = currentSum;                // Then set the biggest sum to max sum
+        }
+    }
+
+    printf("Max sum is = %d\n", maxSum);
+    
     return 0;
 }
