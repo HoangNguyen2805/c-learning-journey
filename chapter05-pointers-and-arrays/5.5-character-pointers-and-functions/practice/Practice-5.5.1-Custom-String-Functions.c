3bf0789c51bf9eb1604ac3fa29f1692ae90c27a2
hmn3146 <hmn3146@mavs.uta.edu>
2026-07-29 16:47:49 -0500

    Ch5.5.1: Fix strcmp - dereference pointer subtraction (*s - *t), clean up function structure

--- chapter05-pointers-and-arrays/5.5-character-pointers-and-functions/practice/Practice-5.5.1-Custom-String-Functions.c
+++ chapter05-pointers-and-arrays/5.5-character-pointers-and-functions/practice/Practice-5.5.1-Custom-String-Functions.c
@@ -119,9 +119,9 @@ strcmp(s1, s2):
 */
 #include <stdio.h>
 
-int strlen(char *arr);
-void strcpy(char *arr, char *clonearr);
-int strcmp(char *A, char *B);
+int my_strlen(char *arr);
+void my_strcpy(char *A, char *B);
+int my_strcmp(char *A, char *B);
 
 int main(){
 
@@ -129,27 +129,70 @@ int main(){
     int choice;
     do {
         // DISPLAY MENU
-        printf("\n=== String Functions Menu ===\n");
-        printf("1. Test strlen\n");
-        printf("2. Test strcpy\n");
-        printf("3. Test strcmp\n");
+        printf("\n\n=== String Functions Menu ===\n\n");
+        printf("1. Test strlen - String Length\n");
+        printf("2. Test strcpy -  String Copy\n");
+        printf("3. Test strcmp - String Compare\n");
         printf("4. Exit\n");
         printf("Choose an option: ");
-        scanf("%d", &choice);
+        scanf(" %d", &choice);
 
         char arr[100];
+        char clonearr[100];
+        char arrA[100];
+        char arrB[100];
+        
         // PROCESS CHOICE
         switch(choice){
             case 1:
-                printf("Enter your string: ");
-                scanf("%[^\n]", arr);
+              printf("Enter your string: ");
+              scanf(" %[^\n]", arr);
+              printf("The length of your string is %d character long, including white space.", my_strlen(arr));
+              break;
 
-            case 2:
-            // should we use malloc
-
+            case 2:             // should we use malloc
+              printf("Enter your string: arr = ");
+              scanf(" %[^\n]", arr);
+              my_strcpy(clonearr, arr);
+              printf("Copy arr to clonearr, clone arr = %s\n", clonearr);
+              break;
+              
             case 3:
+              printf("Enter your first string: ");
+              scanf(" %[^\n]", arrA);
+              printf("Enter your second string: ");
+              scanf(" %[^\n]", arrB);
+              
+              printf("\nComparing 2 Strings ...\n");
+              int result = my_strcmp(arrA, arrB);
+
+              /*
+              strcmp() DOESNT comapre string length 
+              - It convert both character into number then compare each character by it number value.
+              The value go up From the begging of the alphabet to the end of alphabet.
+              Ex:
+              a is the smallest when conver to number
+              z is the biigest when converting to number.
+
+              So sometime we might see:
+                          "xwqxqxqedq" vs "qxqwqqxqcqecqeceqc"
+
+                          First chars: 'x' vs 'q'ç
+                        120 vs 113
+                        120 > 113 → First string is BIGGER ✓
+              */
+              if(result == 0){
+                printf("First string and Second string are EQUAL!!!\n");
+              } else if(result > 0){ // result = positive , A is BIGGER
+                printf("First string are BIGGER than Second string.\n");
+              } else {               // result = negative , A is SMALLER
+                printf("First string are SMALLER than Second string.\n");
+              }
+              break;
 
             case 4:
+              printf("Existing...\n");
+              break;
 
             default:
             printf("Invalid choice, try again!!!\n");
@@ -163,11 +206,11 @@ int main(){
 
 // Function 1 - strlen() - Strlen Length - How many letter is in the string?
 // the function is to counting letter but we'll return integer , so it int function.
-int strlen(char *arr){
+int my_strlen(char *arr){
     // to count and looping amount base on entry (input)
     // In c , all string end with \0 so , we'll looping in array until we reach \0
     int count = 0;
-    while(arr != 0){
+    while(*arr != '\0'){
         // we'll count first and move to the left of the array after count.
         // if move first before count, we'll miss count index 0. 
 
@@ -187,14 +230,14 @@ int strlen(char *arr){
 // Function 2 - strcpy() - String Copy - How to copy letter from an array to another array.
 // the function will move letter from this array to another, we don't return anything, just 
 // moving pointer to pointer so we will use void fuinction
-void strcpy(char *arr, char *clonearr){
+void my_strcpy(char *A, char *B){
     // to coppy every element from arr to clonearr, we need to point at every element 
     // using while loop, while arr not reach \0.
     // point at address of arr (derefence arr)
     // set the address of arr to adress of clonearr , thats all
     // each while loop arr move right 1 by increment, clone arr also have to move the same because we're coping.
     // loop until arr reach \0, we also copy \0 from arr to clonearr
-    while(*arr++ = *clonearr++){
+    while((*A++ = *B++)){ // In this case B = user input
         ;
     } // when clonearr reach \0 , loop stop, but why ?
     // condition is while arr = clonearr, each elemnt consider TRUE to keep the loop going.
@@ -224,22 +267,23 @@ Notice that A is ALWAY - Subtract B
 Never B - A
 */
 // Either way the function will return the number so it's integer.
-int strcmp(char *A, char *B){
+int my_strcmp(char *A, char *B){
     // while comparring A == B
     while(*A == *B){
         // If equal then A - B = 0, so the loop help read A
         // and find out that all element of A same as B till 
         // the end of A which is \0, so if equal till the end , return 0
         if(*A == '\0'){
-            return ; // return the same
             // mean while increment to the right index on both
             A++;
             B++;
         }
+        return 0; // return 0 the same
         // While check A == B , i some element is different
         // return the different in from of A - B
-        return A - B; // return the different
-    }
+      }
+      return *A - *B; // return the different , IF A - B = Positive number, -> A is BIGGER
+                    //                        IF A - B = Negative number, -> A is SMALLER
 } // The if stament will increment  the location to the right
   // As long as A == B still, 
   // if see the diffrent if statement will end and return the different
